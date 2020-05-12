e043d101860cc510882d7802fb68498b8c1dd382
xXNurioXx <xxnurioxx@gmail.com>
2020-05-12 02:58:22 +0200

    Updated Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 
 class Button extends React.Component {
     render() {
-        return <a class="Button btn btn-success my-2 my-sm-0" href={this.props.href} role="button">
+        return <a className="Button btn btn-success my-2 my-sm-0" href={this.props.href} role="button">
             {this.props.children}
         </a>
     }

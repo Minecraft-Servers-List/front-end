72a8a03adf77fd07c4351229e6a17933f04ffbd5
xXNurioXx <xxnurioxx@gmail.com>
2020-05-12 02:59:52 +0200

    Aded Navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import "./style.css";
+import siteLogo from './site-logo.png';
+
+import { faPlusSquare, faSearch, faServer } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+class Navbar extends React.Component {
+    render() {
+        return <nav className="Navbar navbar navbar-expand navbar-dark bg-kuro">
+            <div className="container">
+                <a className="navbar-brand" href="#">
+                    <img src={siteLogo} width="30" height="30" className="site-logo d-inline-block align-top" alt="" />
+                    Minecraft Server List
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarsExample02">
+                    <ul className="navbar-nav ml-auto">
+
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#addserver">
+                                <FontAwesomeIcon icon={faPlusSquare} /> Add Server
+                                <span className="sr-only">(current)</span>
+                            </a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="#search">
+                                <FontAwesomeIcon icon={faSearch} /> Advanced Search
+                            </a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <FontAwesomeIcon icon={faServer} /> Server List
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    }
+}
+
+export default Navbar;
